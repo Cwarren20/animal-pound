b41c6e2cd2e5514a6f097c5da3eef08115e88db2
cwarren20 <calvindwarren2018@gmail.com>
2020-11-06 15:44:44 -0500

    Cool

--- src/main/java/com/codedifferently/animal/pound/MainApplication.java
+++ src/main/java/com/codedifferently/animal/pound/MainApplication.java
@@ -4,4 +4,14 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Lets make some doggies");
     }
+package com.codedifferently.animal.pound;
+
+    public class MainApplication {
+        public static void main(String[] args){
+            Cow calvin = new Cow(name: "Calvin",new date(): Id);
+            System.out.println("How does a cow scare another cow?? ");
+
 }
+
+
+
